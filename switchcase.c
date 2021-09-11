785be7d64fc2c86aeb3266362fc9c17c26f81a98
Ashika1612 <90318556+Ashika1612@users.noreply.github.com>
2021-09-11 15:57:11 +0530

    created switchcase.c

---
+++ switchcase.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	printf("SELECT A NUMBER: \n1 \n2 \n3 \n4 \n5\n");
+	printf("----------------\n");
+	int ch=0;
+	scanf("%d",&ch);
+	switch(ch)
+	{
+		case 1:
+			printf("FOOD ITEM - FRENCH FRIES\nPRICE - Rs.99");
+			break;
+		case 2:
+			printf("FOOD ITEM - BURGER\nPRICE - Rs.129");
+			break;
+		case 3:
+			printf("FOOD ITEM - SANDWICH\nPRICE - Rs.149");
+			break;
+		case 4:
+			printf("FOOD ITEM - PASTA\nPRICE - Rs.179");
+			break;
+		case 5:
+			printf("FOOD ITEM - PIZZA\nPRICE - Rs.239");
+			break;
+		default:
+			printf("INVALID CHOICE");
+	}
+}
